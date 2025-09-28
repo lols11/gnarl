0fcf78feeb2b644359f4708bb62c308117b5f12d
lols11 <lols11@spoko.pl>
2025-09-28 04:32:50 +0200

    Update adc.c

--- src/gnarl/adc.c
+++ src/gnarl/adc.c
@@ -4,6 +4,7 @@
 #include "esp_sleep.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+#include "math.h"
 
 #include "module.h"
 
@@ -13,9 +14,14 @@ const static char *TAG = "ADC";
 
 #define AVG_SAMPLES (10)
 #define AVG_PERIOD (4) // seconds
-#define SCALE_VOLTAGE(raw_mv) (raw_mv * (100 + 220) / 100)
+#define SCALE_VOLTAGE(raw_mv) ((raw_mv) * 2)
+
+//static int learned_charge_voltages[];
+//int learning_voltages[];
+
 static uint16_t full_bat = 4120;
 static uint16_t empty_bat = 3080;
+
 static int battery_voltage;
 
 uint16_t get_battery_voltage(void)
@@ -36,13 +42,12 @@ uint8_t battery_percent(uint16_t battery_voltage)
     double x = (double)(battery_voltage - empty_bat) / (double)(full_bat - empty_bat);
 
 
-    const double a = 13.0; 
-    const double b = 0.70;
-
+    const double a = 15.0;
+    const double b = 3.73;
 
     double y = 1.0 / (1.0 + exp(-a * (x - b)));
 
-    percent = (int) lround(y * 100.0);
+    uint8_t percent = (uint8_t)lround(y * 100.0);
 
     ESP_LOGI(TAG, "(Conv) Battery voltage: %d mV, percentage: %d%%", battery_voltage, percent);
 
@@ -82,19 +87,19 @@ static void adc_task(void *arg)
     ESP_ERROR_CHECK(adc_oneshot_new_unit(&init_config, &adc_handle));
 
     adc_oneshot_chan_cfg_t config = {
-        .atten = ADC_ATTEN_DB_6,
-        .bitwidth = ADC_BITWIDTH_DEFAULT,
+        .atten = ADC_ATTEN_DB_12,
+        .bitwidth = ADC_BITWIDTH_12,
     };
     ESP_ERROR_CHECK(adc_oneshot_config_channel(adc_handle, channel, &config));
 
     adc_cali_handle_t cali_handle;
 
-    adc_cali_line_fitting_config_t cali_config = {
+    adc_cali_curve_fitting_config_t cali_config = {
         .atten = config.atten,
-        .bitwidth = config.bitwidth,
+        .bitwidth = ADC_BITWIDTH_12,
         .unit_id = unit,
     };
-    ESP_ERROR_CHECK(adc_cali_create_scheme_line_fitting(&cali_config, &cali_handle));
+    ESP_ERROR_CHECK(adc_cali_create_scheme_curve_fitting(&cali_config, &cali_handle));
 
     TickType_t last_wake_time = xTaskGetTickCount();
 
@@ -108,25 +113,49 @@ static void adc_task(void *arg)
             ESP_ERROR_CHECK(adc_cali_raw_to_voltage(cali_handle, raw_output, &voltage));
             ESP_LOGI(TAG, "ADC raw: %d, voltage: %d mV", raw_output, voltage);
 
-            if (battery_voltage <= 1000)
-                battery_voltage = SCALE_VOLTAGE(voltage);
+         //   if (battery_voltage <= 1000)
+         //       battery_voltage = SCALE_VOLTAGE(voltage);
 
             averaged_result += voltage;
 
             vTaskDelay(pdMS_TO_TICKS(AVG_PERIOD * 1000 / AVG_SAMPLES));
         }
+       
         averaged_result /= AVG_SAMPLES;
 
         ESP_LOGI(TAG, "ADC voltage: %d mV (%d mV scaled)", averaged_result, SCALE_VOLTAGE(averaged_result));
         battery_voltage = SCALE_VOLTAGE(averaged_result);
 
-        if (battery_voltage < 3000)
+        if (battery_voltage < 3060)
         {
             esp_deep_sleep_start();
         }
 
         vTaskDelayUntil(&last_wake_time, pdMS_TO_TICKS(60000));
     }
+    adc_oneshot_del_unit(adc_handle);
+}
+
+//TODO: Charging curve learning
+static void learn_charging_curve(void){
+    return; //XXX: Temp
+    TickType_t last_wake_time = xTaskGetTickCount();
+ //   if (learning_voltages)
+   // {
+        /* code */
+   // }
+    
+
+    vTaskDelayUntil(&last_wake_time, pdMS_TO_TICKS(61000));
+}
+
+// TODO: Apply Charging curve 
+void batt_curve_init(){
+   // if (learned_charge_voltages == NULL)
+  //  {
+  //      ESP_LOGW(TAG, "Battery charge curve missing, learn when possible");
+  //  }
+    
 }
 
 void adc_init(void)
